8faf3c4725af868361356edfd3b4304305490e6e
Bramwel Ochieng <Bramwelochieng@gmail.com>
2018-11-26 21:15:42 +0300

    enhanced the sign in and sign up functionality using databases

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for,flash
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -18,7 +18,7 @@ db.create_all()
 class User(db.Model): ########Database Class#######
         username = db.Column(db.String(80), unique=True,nullable=False, primary_key=True)
         password = db.Column(db.String(120), unique=False, nullable=False,primary_key=False)
-        email = db.Column(db.String(120), unique=True, nullable=False,primary_key=False)
+        email = db.Column(db.String(120), unique=False, nullable=False,primary_key=False)
 
         def __repr__(self):
                 return "<User: {}>".format(self.username)
@@ -32,15 +32,14 @@ class Customer(User):
         def dashboard():
                 return render_template("dashboard1.html")
 
-
+        
         @app.route('/login', methods=['GET','POST'])
         def login():
-                users = User.query.all()  #####research secret keys######
-                for user in users:
-                        if (request.form['password'] ==user.password) and (request.form['username'] == user.username):
+                for user in User.query.all():
+                        if request.form['password'] ==user.password and request.form['username'] == user.username:
                                 return render_template("dashboard1.html")
-                        else:
-                                return render_template('index1.html')
+                else:
+                        return render_template('index1.html')
 
         @app.route('/signup',methods=['GET','POST'])
         def signup():
