67deeb6837ec96e107ea1571766a1fbf31da3214
Bramwel Ochieng <Bramwelochieng@gmail.com>
2018-11-27 19:33:44 +0300

    improved how businesses and clients sign up differently

--- data.py
+++ data.py
@@ -11,14 +11,17 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir, "userdatabase.db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_file
 
+
 db = SQLAlchemy(app)
 db.create_all()
 
 
-class User(db.Model): ########Database Class#######
-        username = db.Column(db.String(80), unique=True,nullable=False, primary_key=True)
+class User(db.Model): ########Database Class for customers#######
+        id = db.Column('User_id', db.Integer, primary_key = True)
+        username = db.Column(db.String(80), unique=True, nullable=False, primary_key=False)
         password = db.Column(db.String(120), unique=False, nullable=False,primary_key=False)
         email = db.Column(db.String(120), unique=False, nullable=False,primary_key=False)
+        location = db.Column(db.String(120), unique=False, nullable=False,primary_key=False)
 
         def __repr__(self):
                 return "<User: {}>".format(self.username)
@@ -44,17 +47,28 @@ class Customer(User):
         @app.route('/signup',methods=['GET','POST'])
         def signup():
                 return render_template("index2.html")
-
-
+        
         @app.route('/sign up',methods=['GET','POST'])
-        def moreusers():
-                user = User(username=request.form["username"],password=request.form["password"],email=request.form['addrress'])
-                db.session.add(user)
-                db.session.commit()
-                return render_template('dashboard1.html')
+        def signupcustorbiz():
+                return render_template('index3.html')
+
+
+        @app.route('/sign up client',methods=['GET','POST'])
+        def Customer_signup():
+                if request.form:
+                        if request.form['password'] != request.form['password2']:
+                                return render_template('index3.html')
+                        else:
+                                user = User(username=request.form["username"],password=request.form["password"],email=request.form['email'],location=request.form['location'])
+                                db.session.add(user)
+                                db.session.commit()
+                                return render_template('dashboard1.html')
 
         @app.route("/logout")
         def logout():        
                 return render_template("index.html")
+
+
+
 
 
