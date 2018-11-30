28253ce3d922264f84c7be738b1d97b9752ad423
Bramwel Ochieng <Bramwelochieng@gmail.com>
2018-11-30 10:35:26 +0300

    enhanced user registration using flask-wtf

--- data.py
+++ data.py
@@ -4,7 +4,14 @@ from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from forms import LoginForm,SignUpForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager,UserMixin, current_user, login_user, logout_user, login_required
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email,EqualTo
+
 from config import Config
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,16 +20,18 @@ database_file = "sqlite:///{}".format(os.path.join(project_dir, "userdatabase.db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_file
 app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
 
 db = SQLAlchemy(app)
 db.create_all()
 migrate = Migrate(app, db)
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(64), index=True, unique=True)
         email = db.Column(db.String(120), index=True, unique=True)
@@ -31,6 +40,12 @@ class User(db.Model):
 
         def __repr__(self):
                 return '<User {}>'.format(self.username)
+        
+        def set_password(self, password):
+                self.password_hash = generate_password_hash(password)
+
+        def check_password(self, password):
+                return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
         id = db.Column(db.Integer, primary_key=True)
@@ -41,11 +56,35 @@ class Post(db.Model):
         def __repr__(self):
                 return '<Post {}>'.format(self.body)
 
+class RegistrationForm(FlaskForm):
+        username = StringField('Username', validators=[DataRequired()])
+        email = StringField('Email', validators=[DataRequired(), Email()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        password2 = PasswordField(
+                'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+        submit = SubmitField('Register')
+
+        def validate_username(self, username):
+                user = User.query.filter_by(username=username.data).first()
+                if user is not None:
+                        raise ValidationError('Please use a different username.')
+
+        def validate_email(self, email):
+                user = User.query.filter_by(email=email.data).first()
+                if user is not None:
+                        raise ValidationError('Please use a different email address.')
+
+class LoginForm(FlaskForm):
+        username = StringField('Username', validators=[DataRequired()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        remember_me = BooleanField('Remember Me')
+        submit = SubmitField('Sign In')
+
 class Customer(User):
         @app.route("/")
+        @login_required         ###redirects and url for() improvements
         def index():
-                form = LoginForm()
-                return render_template("index.html",form=form)
+                return render_template("dashboard1.html")
         
         @app.route("/dashboard")  ###redirects and url for() improvements
         def dashboard():
@@ -54,41 +93,43 @@ class Customer(User):
         
         @app.route('/login', methods=['GET','POST'])
         def login():           ##form validators,flash messages,, improve error validation on login forms
+                if current_user.is_authenticated:
+                        return render_template("dashboard1.html")
                 form = LoginForm()
-                for user in User.query.all():
-                        if request.form['password'] ==user.password and request.form['username'] == user.username:
-                                flash('Welcome {}'.format(user.username))
-                                return render_template("dashboard1.html")
-                else:
-                        return render_template('index1.html',form=form)
+                if form.validate_on_submit():
+                        user = User.query.filter_by(username=form.username.data).first()
+                        if user is None or not user.check_password(form.password.data):
+                                flash('Invalid username or password')
+                                return render_template("index.html",form=form)
+                        login_user(user, remember=form.remember_me.data)
+                        return render_template("dashboard1.html")
+                return render_template('index.html', title='Sign In', form=form)
 
-        @app.route('/signup',methods=['GET','POST'])
-        def signup():
-                return render_template("index2.html")
         
         @app.route('/sign up',methods=['GET','POST'])
         def signupcustorbiz():
-                form=SignUpForm
+                form=RegistrationForm()
                 return render_template('index3.html',form=form)
 
+        @app.route('/sign_up_client', methods=['GET','POST'])
+        def register():
+                if current_user.is_authenticated:
+                        return redirect(url_for('dashboard'))
+                form = RegistrationForm()
+                if form.validate_on_submit():
+                        user = User(username=form.username.data, email=form.email.data)
+                        user.set_password(form.password.data)
+                        db.session.add(user)
+                        db.session.commit()
+                        
+                        flash('Congratulations, you are now a registered user!')
+                        return render_template('dashboard1.html')
+                return render_template('index3.html', form=form)
 
-        @app.route('/sign up client',methods=['GET','POST'])
-        def Customer_signup():
-                if request.form:
-                        form=SignUpForm
-                        if request.form['password'] != request.form['password2']:
-                                return render_template('index3.html')
-                        else:
-                                user = User(username=request.form["username"],password=request.form["password"],email=request.form['email'],location=request.form['location'])
-                                db.session.add(user)
-                                db.session.commit()
-                                return render_template('dashboard1.html')
-
-        @app.route("/logout")
+        @app.route('/logout')
         def logout():
-                form = LoginForm()       
-                return render_template("index.html",form=form)
-
+                logout_user()
+                return render_template("dashboard1.html")
 
 
 
