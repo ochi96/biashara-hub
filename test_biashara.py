7303df01853e3c3ea8c7656efe18e2b68f0f1bd9
Bramwel Ochieng <Bramwelochieng@gmail.com>
2018-12-07 12:11:12 +0300

    added a test

--- test_biashara.py
+++ test_biashara.py
@@ -2,7 +2,7 @@ import os
 
 import tempfile
 import pytest
-from biashara import app
+from biashara.control import app
 
 
 @pytest.fixture
---
+++ tests/unit/test_models.py
@@ -0,0 +1,12 @@
+from biashara import User
+ 
+ 
+def test_new_user():
+    """
+    GIVEN a User model
+    WHEN a new User is created
+    THEN check the email, hashed_password, authenticated, and role fields are defined correctly
+    """
+    user = User()
+    assert user.set_password == 'patkennedy79@gmail.com'
+    assert new_user.hashed_password != 'FlaskIsAwesome
