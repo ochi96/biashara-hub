b20adb875ceaecfa5da10961d654f0676d7bdf06
Bramwel Ochieng <Bramwelochieng@gmail.com>
2018-12-07 18:11:53 +0300

    added migrations directory

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+
+MIGRATION_DIR = os.path.join('biashara-hub', 'migrations')
 
 #from app import app, db
 from biashara import app
 from biashara.models import db
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db,directory=MIGRATION_DIR)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
